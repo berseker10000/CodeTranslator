984d6e1156b0dbf8009297b697bbcd619ec1c618
rouquette <rouquette.loic@gmail.com>
2015-09-25 12:58:23 +0200

    Ajout de la documentation

---
+++ phpdoc/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Algorithm"],["c","Connection"],["c","container"],["f","convertType()"],["c","Dictionnary"],["f","extracting()"],["f","functionSeeker()"],["f","getTranslationLayout()"],["f","interpreting()"],["c","layout"],["c","Mailbox"],["c","Mail "],["c","Translation"],["f","translation()"],["c","User"],["f","userFunction()"]];
