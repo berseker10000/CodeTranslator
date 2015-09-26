9a8b65e94125552f027db71d95f6fc96022d664c
loic <rouquette.loic@gmail.com>
2015-09-26 18:25:40 +0200

    Test des Classes

--- phpdoc/elementlist.js
+++ phpdoc/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Algorithm"],["c","Connection"],["c","container"],["f","convertType()"],["c","Dictionnary"],["c","EMail"],["f","extracting()"],["f","functionSeeker()"],["f","getTranslationLayout()"],["f","interpreting()"],["c","Layout"],["c","Mailbox"],["c","Translation"],["f","translation()"],["c","User"],["f","userFunction()"]];
+ApiGen.elements = [["c","Algorithm"],["c","Connection"],["c","container"],["f","convertType()"],["c","Dictionnary"],["c","EMail"],["f","extracting()"],["f","functionSeeker()"],["f","getTranslationLayout()"],["f","interpreting()"],["c","Layout"],["c","LayoutCollection"],["c","Mailbox"],["c","Translation"],["f","translation()"],["c","User"],["f","userFunction()"]];
